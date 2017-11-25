3955b51fe12134548a29d0813dfed887706c3918
Roslyn Harris <rharris0113@gmail.com>
2017-11-24 19:32:48 -0800

    php action not working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function validateForm() {
 	var complete = document.forms["theForm"]["fname"].value;
-	if (complete== "Thank you1") {
+	if (complete== "Thank you") {
 		alert("Must be filled out");
 		return false;
 	}
