389cce44115860cac6fab0bedb17d5a797f50d18
Roslyn Harris <rharris0113@gmail.com>
2017-11-25 19:27:21 -0800

    added last name to form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function validateForm() {
-	var complete = document.forms["theForm"]["fname"].value;
+	var complete = document.forms["theForm"]["fname"][lname].value;
 	if (complete== "Thank you") {
 		alert("Must be filled out");
 		return false;
